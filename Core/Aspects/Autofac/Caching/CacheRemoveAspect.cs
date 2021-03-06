9c8ca1ce17b2541f5a4584a5d86106c7a3e0f3ac
İbrahim Doğuş <idogus@gmail.com>
2021-03-06 11:05:20 +0300

    Added Cache, Transaction and Performance Aspects

---
+++ Core/Aspects/Autofac/Caching/CacheRemoveAspect.cs
@@ -0,0 +1,27 @@
+﻿using Castle.DynamicProxy;
+using Core.CrossCuttingConserns.Caching;
+using Core.Utilities.Interceptors;
+using Core.Utilities.IoC;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Aspects.Autofac.Caching
+{
+    public class CacheRemoveAspect : MethodInterception
+    {
+        private readonly string _pattern;
+        private readonly ICacheManager _cacheManager;
+
+        public CacheRemoveAspect(string pattern)
+        {
+            _pattern = pattern;
+            _cacheManager = ServiceTool.ServiceProvider.GetService<ICacheManager>();
+        }
+        protected override void OnSuccess(IInvocation invocation)
+        {
+            _cacheManager.RemovePattern(_pattern);
+        }
+    }
+}
--- Core/DependencyResolvers/CoreModule.cs
+++ Core/DependencyResolvers/CoreModule.cs
@@ -1,8 +1,11 @@
-﻿using Core.Utilities.IoC;
+﻿using Core.CrossCuttingConserns.Caching;
+using Core.CrossCuttingConserns.Caching.Microsoft;
+using Core.Utilities.IoC;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
 
 namespace Core.DependencyResolvers
@@ -11,7 +14,10 @@ namespace Core.DependencyResolvers
     {
         public void Load(IServiceCollection services)
         {
+            services.AddMemoryCache();
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddSingleton<ICacheManager, MemoryCacheManager>();
+            services.AddSingleton<Stopwatch>();
         }
     }
 }
